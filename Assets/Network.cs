32b31e294f356a5bfa53f1c6e52b5f9e5e1d352e
Scott Bromander <scott@primeacademy.io>
2016-10-06 19:40:31 -0500

    client connection with custom event

---
+++ Assets/Network.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class Network : MonoBehaviour {
+
+	static SocketIOComponent socket;
+
+	// Use this for initialization
+	void Start () {
+		socket = GetComponent<SocketIOComponent> ();
+		socket.On ("open", OnConnected);
+	}
+
+	void OnConnected(SocketIOEvent e){
+		Debug.Log("connected");
+		socket.Emit("move");
+	}
+}
--- Server/app.js
+++ Server/app.js
@@ -1,5 +1,9 @@
 var io = require('socket.io')(process.env.PORT || 5000);
 
 io.on('connection', function(socket){
-    console.log('client connected', socket);
+    console.log('client connected');
+
+    socket.on('move', function(data){
+      console.log("Client Moved!");
+    });
 });
