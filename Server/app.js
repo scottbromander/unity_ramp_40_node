d9b7a25ac6d43c28d550f4ae1b8f351ff36b4e95
Scott Bromander <scott@primeacademy.io>
2016-10-08 21:13:59 -0500

    player connect and disconnect

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,24 @@
 var io = require('socket.io')(process.env.PORT || 5000);
 
+var playerCount = 0;
+
 io.on('connection', function(socket){
     console.log('client connected, broadcasting spawn');
 
     socket.broadcast.emit('spawn');
+    playerCount++;
+
+    for(var i = 0; i < playerCount; i++){
+      socket.emit('spawn');
+      console.log('sending spawn to new player');
+    }
 
     socket.on('move', function(data){
       console.log("Client Moved!");
+    });
+
+    socket.on('disconnect', function(){
+      console.log("Client Disconnect");
+      playerCount--;
     });
 });
