ddae1941ff3569362fc110cdbb73aa89d2f72212
Scott Bromander <scott@primeacademy.io>
2016-11-18 21:11:29 -0600

    master object

---
+++ Assets/Scripts/MasterObjectScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterObjectScript : MonoBehaviour {
+
+	public GameObject objectOne;
+	public GameObject objectTwo;
+	public GameObject objectThree;
+
+	private GameObject currentObject = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeObject(string shapeObject){
+		if(currentObject != null){
+			Destroy (currentObject);
+		}
+
+		if(shapeObject == "one"){
+
+		}
+	}
+}
