6df1ff16b072f7aba2df38ce15206cf4b4f19e82
Scott Bromander <scott@primeacademy.io>
2016-11-20 20:14:24 -0600

    test for broadcast

--- Server/app.js
+++ Server/app.js
@@ -41,14 +41,14 @@ var playerCount = 0;
 
 io.on('connection', function(socket){
     console.log('client connected, broadcasting spawn');
-
-    socket.broadcast.emit('spawn');
-    playerCount++;
-
-    for(var i = 0; i < playerCount; i++){
-      socket.emit('spawn');
-      console.log('sending spawn to new player');
-    }
+    io.emit('CMS EVENT', { data: "one"});
+    // socket.broadcast.emit('spawn');
+    // playerCount++;
+    //
+    // for(var i = 0; i < playerCount; i++){
+    //   socket.emit('spawn');
+    //   console.log('sending spawn to new player');
+    // }
 
     socket.on('move', function(data){
       console.log("Client Moved!");
